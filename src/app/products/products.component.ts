bd5d4f960e2989ab28523bbfed08aac750f8bec1
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-10-08 09:25:19 +0300

    update

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../products.service';
 import { products } from '../product';
-declare let $:any
+declare let $: any
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -11,30 +11,30 @@ declare let $:any
 export class ProductsComponent {
 
   productId!: string;
-  productImg0!:[]
-  productImg1!:[]
-  productImg2!:[]
-  product!:products
+  productImg0!: []
+  productImg1!: []
+  productImg2!: []
+  product!: products
 
-  constructor(private _ActivatedRoute: ActivatedRoute, private _ProductsService: ProductsService) { 
+  constructor(private _ActivatedRoute: ActivatedRoute, private _ProductsService: ProductsService) {
 
 
-    $(document).ready(function(){
+    $(document).ready(function () {
       $(".owl-carousel").owlCarousel(
         {
-          
-          responsive:{
-              0:{
-                  items:1
-              },
-              600:{
-                  items:1
-              },
-              1000:{
-                  items:1
-              }
+
+          responsive: {
+            0: {
+              items: 1
+            },
+            600: {
+              items: 1
+            },
+            1000: {
+              items: 1
+            }
           }
-      }
+        }
       );
     });
   }
@@ -44,10 +44,13 @@ export class ProductsComponent {
     localStorage.setItem("currentPage", `/products/${this.productId}`)
     this._ProductsService.getSpecProducts(this.productId).subscribe({
       next: (res) => {
-        this.product=res.data
-        this.productImg0=res.data.images[0]
-        this.productImg1=res.data.images[1]
-        this.productImg2=res.data.images[2]
+        this.product = res.data
+        this.productImg0 = res.data.images[0]
+        this.productImg1 = res.data.images[1]
+        this.productImg2 = res.data.images[2]
+        if (this.productImg2 == null) {
+          this.productImg2 = this.productImg1
+        }
       },
       error: (error) => { }
     })
