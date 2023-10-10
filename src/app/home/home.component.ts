655ccbfee80b3df3160176c8166d41c983684d9a
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-10-10 14:03:01 +0300

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,9 +5,7 @@ import { products } from '../product';
 import { WishListService } from '../wish-list.service';
 import { CartService } from '../cart.service';
 
-
 declare let $: any
-
 
 @Component({
   selector: 'app-home',
@@ -21,14 +19,11 @@ export class HomeComponent {
   loaded: boolean = false
   allId: string[] = []
   whishId: string[] = []
-  i!: number;
   intersection: string[] = []
   heart: boolean = true
   loadingCenter: boolean = false;
   userWord: string = ""
-  indexArr: number[] = []
-
-
+  
   constructor(private _Router: Router, private _ProductsService: ProductsService, private _WishListService: WishListService, private _CartService: CartService) {
     $(document).ready(function () {
       $(".owl-carousel").owlCarousel(
@@ -58,38 +53,11 @@ export class HomeComponent {
         this.allProducts = res.data
         this.loaded = true
         this.loading = false
-
-        // res.data.forEach((values: any) => {
-        //   this.allId.push(values._id)
-        // })
-
-
-        // this._WishListService.getUserWishList().subscribe({
-        //   next: (res) => {
-        //     res.data.forEach((values: any) => {
-        //       this.whishId.push(values._id)
-        //     })
-        //     console.log(this.whishId); 
-        //      console.log(this.allId);
-
-        //      this.whishId.forEach( array1Ttem => {
-        //       this.allId.forEach( array2Item => {
-        //          if(array1Ttem == array2Item){
-        //          this.heart=true;
-        //         }
-        //         else{
-
-        //         }
-
-        //       })
-        //     })
-        //   }, 
-
-        // })
-        $(document).ready(() => {
-          this.indexArr.push(...JSON.parse(localStorage.getItem("indexArr")!))
-          for (var i = 0; i < this.indexArr.length; i++) {
-            document.querySelectorAll(".fa-heart")[this.indexArr[i]].classList.add("text-danger")
+        this._WishListService.getUserWishList().subscribe({
+          next: (res) => {
+            for (let i = 0; i < res.data.length; i++) {
+              document.getElementById(res.data[i]._id)?.classList.add("text-danger")
+            }
           }
         })
       },
@@ -99,22 +67,18 @@ export class HomeComponent {
     })
   }
 
-
-
   wishListAddAndRemove(pId: string, event: any, i: number) {
     if (Array.from(event.srcElement.classList).includes("text-danger") == false) {
       this._WishListService.addToWishList(pId).subscribe(() => {
-        this.indexArr.push(i)
-        localStorage.setItem("indexArr", JSON.stringify(this.indexArr))
         document.querySelectorAll(".fa-heart")[i].classList.add("text-danger")
       })
     }
     else if (Array.from(event.srcElement.classList).includes("text-danger") == true) {
-      document.querySelectorAll(".fa-heart")[i].classList.remove("text-danger")
+      this._WishListService.deleteItemWishList(pId).subscribe(() => {
+        document.querySelectorAll(".fa-heart")[i].classList.remove("text-danger")
+      })
     }
-
   }
-
   addToCart(pId: string) {
     this.loadingCenter = true
     return this._CartService.addToCart(pId).subscribe({
